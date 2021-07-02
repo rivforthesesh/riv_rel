280eb5c3639b4f9c524fe191da458609917ded9a
riv <81919069+rivforthesesh@users.noreply.github.com>
2021-07-03 00:30:59 +0100

    Add files via upload

--- codebase/riv_rel/riv_rel.py
+++ codebase/riv_rel/riv_rel.py
@@ -936,32 +936,8 @@ def get_direct_relation(sim_x: SimInfoParam, sim_y: SimInfoParam):
 # gens = xy_direct_rels
 # gender bit is for compatibility with inlaw rels
 def format_direct_rel_gender(gens: List, gender: int):
-    gens_str = []
-    for n in gens:
-        rel_str = ''
-        if abs(n) > 1:
-            if abs(n) > 2:
-                rel_str += 'great'
-                if abs(n) > 3:
-                    rel_str += '(x' + str(abs(n) - 2) + ')'
-                rel_str += '-'
-            rel_str += 'grand'
-        if n > 0:
-            if gender:
-                rel_str += 'daughter'
-            else:
-                rel_str += 'son'
-        elif n < 0:
-            if gender:
-                rel_str += 'mother'
-            else:
-                rel_str += 'father'
-        elif n == 0:
-            rel_str = 'self'
-        gens_str.append(rel_str)
-
+    gens_str = rrs.direct(gens, gender)
     riv_log(f'[format direct rel] {gens_str}', 3)
-
     return gens_str
 
 
@@ -1296,74 +1272,8 @@ def get_indirect_relation(sim_x: SimInfoParam, sim_y: SimInfoParam):
 # nb. expected list is the output from get_indirect_rel: [(sim_z, sim_w, nx, ny, sib_strength),...]
 # the gender bit is for making in-law stuff easier
 def format_indirect_rel_gender(xy_indirect_rels: List, gender: int):
-    rels_via = []
-    for boi in xy_indirect_rels:
-        sim_z = boi[0]
-        sim_w = boi[1]
-        nx = boi[2]
-        ny = boi[3]
-        if nx <= 0 or ny <= 0:  # filtering out any Problems
-            continue
-        sib_strength = boi[4]
-        nth = min([nx, ny]) - 1  # nth cousin
-        nce = nx - ny  # n times removed
-        rel_str = ''
-        if sib_strength == 0.5:
-            rel_str = 'half '
-        if nth == 0:  # pibling/sibling/nibling
-            if abs(nce) > 1:
-                if abs(nce) > 2:
-                    rel_str += 'great'
-                    if abs(nce) > 3:
-                        rel_str += '(x' + str(abs(nce) - 2) + ')'
-                    rel_str += '-'
-                rel_str += 'grand'
-            if nce < 0:
-                if gender:
-                    rel_str += 'aunt'
-                else:
-                    rel_str += 'uncle'
-            elif nce == 0:
-                if gender:
-                    rel_str += 'sister'
-                else:
-                    rel_str += 'brother'
-            elif nce > 0:
-                if gender:
-                    rel_str += 'niece'
-                else:
-                    rel_str += 'nephew'
-        else:  # cousin
-            nce = abs(nce)
-            if nth == 1:
-                rel_str += 'first '
-            elif nth == 2:
-                rel_str += 'second '
-            elif nth == 3:
-                rel_str += 'third '
-            else:
-                # nth
-                if nth % 10 == 1 and nth != 11:
-                    rel_str += str(nth) + 'st '
-                elif nth % 10 == 2 and nth != 12:
-                    rel_str += str(nth) + 'nd '
-                elif nth % 10 == 3 and nth != 13:
-                    rel_str += str(nth) + 'rd '
-                else:
-                    rel_str += str(nth) + 'th '
-            rel_str += 'cousin'
-            if nce > 0:
-                if nce == 1:
-                    rel_str += ' once '
-                elif nce == 2:
-                    rel_str += ' twice '
-                else:
-                    rel_str += ' {num} times '.format(num=str(nce))
-                rel_str += 'removed'
-        rels_via.append((rel_str, sim_z, sim_w))
-
+    rels_via = rrs.indirect(xy_indirect_rels, gender)
     riv_log(f'[format indirect rel] {rels_via}', 3)
-
     return rels_via  # [(str, sim_z, sim_w)]
 
 
@@ -1512,42 +1422,8 @@ def get_inlaw_relation(sim_x: SimInfoParam, sim_y: SimInfoParam):
 
 
 def format_inlaw_rel(xy_inlaw_rels: List, sim_x: SimInfoParam):
-    inlaw_rels = []  # this will have the strings and via
-    for rel in xy_inlaw_rels:
-        try:
-            if rel[0] == 0:  # spouse
-                try:
-                    if sim_x.is_female:
-                        inlaw_rels.append(('wife', 0))
-                    else:
-                        inlaw_rels.append(('husband', 0))
-                except Exception as e:
-                    riv_log(f'error in adding/appending spouse relation ({e})')
-            elif rel[0] == 1:  # direct-rel-in-law
-                try:
-                    for drel in format_direct_rel_gender([rel[1]], sim_x.is_female):  # = [str,...]
-                        try:
-                            inlaw_rels.append((drel + ' in law', rel[3]))
-                        except Exception as e:
-                            riv_log(f'error in appending direct-rel-in-law ({e})')
-                except Exception as e:
-                    riv_log(f'error in adding direct-rel-in-law ({e})')
-            elif rel[0] == 2:  # indirect rel-in-law
-                try:
-                    for irel in format_indirect_rel_gender([rel[1]], sim_x.is_female):  # = [(str, sim_z, sim_w),...]
-                        try:
-                            inlaw_rels.append((irel[0] + ' in law', rel[3]))
-                        except Exception as e:
-                            riv_log(f'error in appending indirect-rel-in-law ({e})')
-                except Exception as e:
-                    riv_log(f'error in adding indirect-rel-in-law ({e})')
-            elif rel[0] == -1:  # error
-                inlaw_rels.append((rel[1], 1))
-        except Exception as e:
-            riv_log(f'error in formatting inlaw rels ({e})')
-
+    inlaw_rels = rrs.inlaw(xy_inlaw_rels, sim_x)  # this will have the strings and via
     riv_log(f'[format inlaw rel] {inlaw_rels}', 3)
-
     return inlaw_rels  # [(string, sim)] where sim is the spouse related to sim_x or sim_y, or is 0 if spouse
 
 
@@ -1731,28 +1607,8 @@ def get_step_relation(sim_x: SimInfoParam, sim_y: SimInfoParam):
 
 
 def format_step_rel(xy_step_rels: List, sim_x: SimInfoParam):
-    step_rels = []  # this will have the strings and via
-    for step_rel in xy_step_rels:
-        try:
-            sim_xz = step_rel[0]
-            sim_zy = step_rel[1]
-            rel_list = step_rel[2]
-            for rel in rel_list:
-                try:
-                    if isinstance(rel, int):
-                        rel_str = format_direct_rel([rel], sim_x)[0]
-                        # str from [str]
-                    else:
-                        rel_str = format_indirect_rel([rel], sim_x)[0][0]
-                        # str from [(str, sim_z, sim_w)]
-                    step_rels.append(('step ' + rel_str, sim_xz, sim_zy))
-                except Exception as e:
-                    riv_log(f'error in formatting step rel {rel} ({e})')
-        except Exception as e:
-            riv_log(f'error in formatting step rels {step_rel} ({e})')
-
+    step_rels = rrs.step(xy_step_rels, sim_x)  # this will have the strings and via
     riv_log(f'[format step rel] {step_rels}', 3)
-
     return step_rels  # [(string, sim_xz, sim_zy)]
 
 
